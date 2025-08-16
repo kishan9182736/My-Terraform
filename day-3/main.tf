678e917f609f9f0a7751b4116e8bca985f2483a7
kishan <108215645+kishan9182736@users.noreply.github.com>
2025-08-16 04:11:44 +0000

    Terraform VPC and EC2 setup with Flask app

--- day-3/main.tf
+++ day-3/main.tf
@@ -1,30 +1,67 @@
+# ---------------------------
+# PROVIDER
+# ---------------------------
+
 provider "aws" {
   region = "eu-central-1"
 }
 
+
+# ---------------------------
+# VARIABLES
+# ---------------------------
+
 variable "cidr" {
     default = "10.0.0.0/16"
 }
+
+# ---------------------------
+# SSH KEY PAIR
+# ---------------------------
+
 
 resource "aws_key_pair" "example" {
   key_name = "kishan-ec2-key"
   public_key = file("/home/codespace/.ssh/id_rsa.pub")
 }
 
+
+# ---------------------------
+# VPC
+# ---------------------------
+
 resource "aws_vpc" "myvpc" {
   cidr_block = var.cidr
+  tags = { Name = "my-vpc" }
 }
+
+
+# ---------------------------
+# SUBNET
+# ---------------------------
 
 resource "aws_subnet" "sub1" {
   vpc_id = aws_vpc.myvpc.id
   cidr_block = "10.0.0.0/24"
   availability_zone = "eu-central-1a"
   map_public_ip_on_launch = true 
+  tags = { Name = "subnet-1" }
 }
+
+
+# ---------------------------
+# INTERNET GATEWAY
+# ---------------------------
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.myvpc.id
+  tags   = { Name = "igw" }
 }
+
+
+# ---------------------------
+# ROUTE TABLE
+# ---------------------------
 
 resource "aws_route_table" "RT" {
   vpc_id = aws_vpc.myvpc.id
@@ -33,6 +70,8 @@ resource "aws_route_table" "RT" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
  }
+ 
+ tags = { Name = "rt-main" }
 
 }
 
@@ -41,12 +80,20 @@ resource "aws_route_table_association" "rt1" {
   route_table_id = aws_route_table.RT.id
 }
 
+
+
+# ---------------------------
+# SECURITY GROUP
+# ---------------------------
+
 resource "aws_security_group" "websg" {
 
   name = "web"
   vpc_id = aws_vpc.myvpc.id
-
+  
+ 
   ingress  {
+
     description = "HTTP from VPC"
     from_port = 80
     to_port = 80
@@ -55,6 +102,7 @@ resource "aws_security_group" "websg" {
   }
 
   ingress  {
+
     description = "SSH"
     from_port = 22
     to_port = 22
@@ -63,18 +111,23 @@ resource "aws_security_group" "websg" {
   }
 
  egress  {
-    //description = "HTTP from VPC"
+
+    #Outbound: allow all
     from_port = 0
     to_port = 0
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-tags = {
-  Name = "web-sg"
 
-  }
+tags = { Name = "web-sg" }
 
 }
+
+
+
+# ---------------------------
+# EC2 INSTANCE
+# ---------------------------
 
 resource "aws_instance" "server" {
   
@@ -97,17 +150,14 @@ provisioner "remote-exec"{
 
   inline =[
 
-    "sudo apt update -y",
-    "sudo apt install -y git python3-pip",
-    "git clone https://github.com/kishan9182736/My-Terraform.git /home/ubuntu/My-Terraform",
-    "cd /home/ubuntu/My-Terraform/day-3",
-    "sudo pip3 install flask",
-    "sudo python3 app.py &" 
+      "sudo apt update -y && sudo apt install -y git python3-flask",
+      "git clone https://github.com/kishan9182736/My-Terraform.git /home/ubuntu/My-Terraform",
+      "python3 /home/ubuntu/My-Terraform/day-3/app.py &"
    
    ]
-
+  
  }
-
+  tags = { Name = "web-server" }
 }
 
 

eab1e6d7daf0cad6e0a3a47b65b6780b85dc2e66
kishan <108215645+kishan9182736@users.noreply.github.com>
2025-08-16 02:33:15 +0000

    Add Terraform configs for VPC, subnet, EC2, and security group

---
+++ day-3/main.tf
@@ -0,0 +1,115 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+variable "cidr" {
+    default = "10.0.0.0/16"
+}
+
+resource "aws_key_pair" "example" {
+  key_name = "kishan-ec2-key"
+  public_key = file("/home/codespace/.ssh/id_rsa.pub")
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.cidr
+}
+
+resource "aws_subnet" "sub1" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "eu-central-1a"
+  map_public_ip_on_launch = true 
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+
+ route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+ }
+
+}
+
+resource "aws_route_table_association" "rt1" {
+  subnet_id = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_security_group" "websg" {
+
+  name = "web"
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress  {
+    description = "HTTP from VPC"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress  {
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ egress  {
+    //description = "HTTP from VPC"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+tags = {
+  Name = "web-sg"
+
+  }
+
+}
+
+resource "aws_instance" "server" {
+  
+  ami = "ami-02003f9f0fde924ea"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.websg.id]
+  subnet_id = aws_subnet.sub1.id
+
+  connection {
+
+      type = "ssh"
+      user = "ubuntu"
+      private_key = file("/home/codespace/.ssh/id_rsa")
+      host = self.public_ip
+  } 
+
+
+provisioner "remote-exec"{
+
+  inline =[
+
+    "sudo apt update -y",
+    "sudo apt install -y git python3-pip",
+    "git clone https://github.com/kishan9182736/My-Terraform.git /home/ubuntu/My-Terraform",
+    "cd /home/ubuntu/My-Terraform/day-3",
+    "sudo pip3 install flask",
+    "sudo python3 app.py &" 
+   
+   ]
+
+ }
+
+}
+
+
+
+
